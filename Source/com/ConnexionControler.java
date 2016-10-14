008b6bf4daae5b6497ea371a470d04bf7f5ce462
mickaelreil <michael.reilhac@live.fr>
2016-10-14 11:37:00 +0200

    Update ConnexionControler.java

--- Source/com/ConnexionControler.java
+++ Source/com/ConnexionControler.java
@@ -1,3 +1,13 @@
+import java.sql.*;
+
 public class ConnexionControler {
-    ConnexionPanel view;
+
+    public ConnexionControler(String nom, String reference) throws SQLException {
+	ClientModel c = new ClientModel();
+	if(c.getClient(0,null,nom,reference)){
+	    System.out.println("ok");
+	    View.next();
+	}
+
+    }
 }
