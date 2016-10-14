fdaf0435d3e0d5b98185a0867880decd3b28a555
projetphpsen <simonvasseur91550@live.com>
2016-10-14 10:14:33 +0200

    rm homeview

--- Source/com/View/Fenetre.java
+++ Source/com/View/Fenetre.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 public class Fenetre extends JFrame {
-    private JPanel siege = new EndPanel();
+    private JPanel siege = new MenuPanel();
     public Fenetre() {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setMinimumSize(new Dimension(1000, 800));
